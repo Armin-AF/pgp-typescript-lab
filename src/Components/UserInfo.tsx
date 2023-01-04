175a66bebcaf03c4379fe623f3227ab1c7c1f678
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-01-04 12:57:21 +0100

    User info implements

---
+++ src/Components/UserInfo.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+interface User {
+    name: {
+        first: string;
+        last: string;
+    };
+    age: number;
+    email: string;
+}
+
+interface UserInfoState {
+    user: User | null;
+}
+
+class UserInfo extends React.Component<{}, UserInfoState> {
+    state: UserInfoState = {
+        user: null
+    };
+
+    componentDidMount() {
+        this.fetchUser();
+    }
+
+    fetchUser = () => {
+        fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(data => {
+                const user = data.results[0];
+                this.setState({ user });
+            });
+    }
+
+    render() {
+        const { user } = this.state;
+
+        if (!user) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div>
+                <h2>User Information</h2>
+                <p>Name: {`${user.name.first} ${user.name.last}`}</p>
+                <p>Age: {user.age}</p>
+                <p>Email: {user.email}</p>
+            </div>
+        );
+    }
+}
+
+export default UserInfo;
